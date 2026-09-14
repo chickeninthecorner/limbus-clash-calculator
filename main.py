6ca2cca88e3516a9a868812aa8326eace5512116
Luventius Braven Widjaya <luventiusbraven@gmail.com>
2026-09-15 02:18:54 +0700

    changed skill class to normal coin skill

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def combination(n, r):
         combination_dict[(n, r)] = ans
         return ans
 
-class Skill:
-    def __init__(self, base_power, coin_count, coin_power):
+class NormalCoinSkill:
+    def __init__(self, base_power, coin_count, coin_power, sanity):
         self.base_power = base_power
         self.coin_count = coin_count
         self.coin_power = coin_power
-
+        self.sanity = sanity
