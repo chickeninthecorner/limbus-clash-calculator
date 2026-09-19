a235536dbe4f016e97b155fee509a746d96cbd36
Luventius Braven Widjaya <luventiusbraven@gmail.com>
2026-09-20 03:01:03 +0700

    made red coins in UI reflect red coins in actual calculations

--- web.py
+++ web.py
@@ -5,18 +5,38 @@ from main import clash, Skill
 def update_all(event=None):
 	base_power_1 = int(document.querySelector("#base-power-1").value)
 	coin_power_1 = int(document.querySelector("#coin-power-1").value)
-	coin_count_1 = int(document.querySelector("#coin-count-1").value)
 	sanity_1 = int(document.querySelector("#sanity-1").value)
 	paralysis_1 = int(document.querySelector("#paralysis-1").value)
 
+	coins_1 = []
+
+	container = document.querySelector("#circle-container-1")
+	contained_coins = container.querySelectorAll(".coin")
+
+	for coin in contained_coins:
+		if coin.classList.contains("yellow"):
+			coins_1.append('N')
+		elif coin.classList.contains("red"):
+			coins_1.append('R')
+
 	base_power_2 = int(document.querySelector("#base-power-2").value)
 	coin_power_2 = int(document.querySelector("#coin-power-2").value)
-	coin_count_2 = int(document.querySelector("#coin-count-2").value)
 	sanity_2 = int(document.querySelector("#sanity-2").value)
 	paralysis_2 = int(document.querySelector("#paralysis-2").value)
 
-	skill1 = Skill(base_power_1, ('N') * coin_count_1, coin_power_1, sanity_1, paralysis_1)
-	skill2 = Skill(base_power_2, ('N') * coin_count_2, coin_power_2, sanity_2, paralysis_2)
+	coins_2 = []
+	
+	container = document.querySelector("#circle-container-2")
+	contained_coins = container.querySelectorAll(".coin")
+
+	for coin in contained_coins:
+		if coin.classList.contains("yellow"):
+			coins_2.append('N')
+		elif coin.classList.contains("red"):
+			coins_2.append('R')
+
+	skill1 = Skill(base_power_1, tuple(coins_1), coin_power_1, sanity_1, paralysis_1)
+	skill2 = Skill(base_power_2, tuple(coins_2), coin_power_2, sanity_2, paralysis_2)
 
 	try:
 		result = clash(skill1, skill2, 0)
